faef313c574d185bb5fe3ce7f35b75a5fe89b551
GuilhermeFranceschini <guilhermefranceschinijesuino8@gmail.com>
2026-09-25 18:38:54 -0300

    Saída de dados melhorada

--- checker.py
+++ checker.py
@@ -6,15 +6,22 @@ security_headers = [
     "X-Frame-Options",
     "Referrer-Policy"
 ]
+print("------------------------------------------------------")
+print("|Bem-vindo ao verificador de cabeçalhos de segurança!|")
+print("------------------------------------------------------")
 url = input("Digite a URL desejada:")
 def check_website(url):
     try:
+        print(f"Verificando os cabeçalhos de segurança para {url}...")
         response = requests.get(url, timeout = 5)
+        headers_presentes = 0
         for header in security_headers:
             if header in response.headers:
-                print(f"{header} está presente.")
+                print(f"{header} [✓] - presente.")
+                headers_presentes += 1
             else:
-                print(f"{header} não está presente.")
+                print(f"{header} [X] - ausente.")
+        print(f"Total de cabeçalhos de segurança presentes: {headers_presentes}")
     except requests.RequestException as e:
         print(f"Erro ao acessar o site: {e}")
 check_website(url)
